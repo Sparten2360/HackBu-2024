3bd086b42668bc0819d5045f25758e4ae5d734bb
shawn <skhilla1@binghamton.edu>
2024-02-17 17:39:11 -0500

    Website Base

--- StudySpace.js
+++ StudySpace.js
@@ -1,1 +1,9 @@
 console.log('hello world');
+function buttonClicked() {
+    var x = document.createElement("BUTTON");
+    // making a image into a button using java script
+    let count = 0;
+    var t = document.createTextNode(count += 1);
+    x.appendChild(t);
+    document.body.appendChild(x);
+}
